3354ab72cc172a0eba3543e0a5fa07d203d7686c
manjeet <manjeetkarnwal1027@gmail.com>
2024-07-15 21:19:20 +0530

    position of text in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 function Footer() {
   return (
     <div>
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top bg-success ">
+      <footer className="d-flex flex-wrap justify-content-end align-items-center py-3 my-4 border-top bg-success ">
     <div className="col-md-4 d-flex align-items-center text-white fs-5">
       <Link to="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
       </Link>
