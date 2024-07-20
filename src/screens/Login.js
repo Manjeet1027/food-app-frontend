a0deea28f6a3720d2703a31909dac8958338eacc
manjeet <manjeetkarnwal1027@gmail.com>
2024-07-20 13:59:29 +0530

    animation on the forms

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -41,23 +41,25 @@ function Login() {
   return (
 
     <div className="user-form">
-    <div className="container ">
-      <form onSubmit={handleSubmit}>
+      <div className="container out-box">
+        <div className="in-box ">
+          <form onSubmit={handleSubmit}>
 
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" value={credentials.email} onChange={onChange} placeholder="e.g. sample@gmail.com" />
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" name="password" value={credentials.password} onChange={onChange} placeholder="e.g. 123456" />
-        </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" value={credentials.email} onChange={onChange} placeholder="e.g. sample@gmail.com" />
+              <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+              <input type="password" className="form-control" id="exampleInputPassword1" name="password" value={credentials.password} onChange={onChange} placeholder="e.g. 123456" />
+            </div>
 
-        <button type="submit" className="fs-5 m-3 p-2  btn btn-success">Submit</button>
-        <Link to="/createuser" className="fs-5 m-3 p-2  btn btn-danger" >I'm a new user</Link>
-      </form>
-    </div>
+            <button type="submit" className="fs-5 m-3 p-2  btn btn-success">Submit</button>
+            <Link to="/createuser" className="fs-5 m-3 p-2  btn btn-danger" >I'm a new user</Link>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -47,31 +47,33 @@ function Signup() {
   return (
 
     <div className="user-form">
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
-          <input type="text" className="form-control" name="name" value={credentials.name} onChange={onChange} placeholder="Name must be atleast of 5 characters" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" value={credentials.email} onChange={onChange} placeholder="e.g. sample@gmail.com" />
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" name="password" value={credentials.password} onChange={onChange} placeholder="Password must be atleast of 5 characters" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Address</label>
-          <input type="text" className="form-control" id="exampleInputPassword1" name="geolocation" value={credentials.geolocation} onChange={onChange} placeholder="e.g. Delhi"/>
-        </div>
+      <div className="container out-box">
+        <div className="in-box">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
+              <input type="text" className="form-control" name="name" value={credentials.name} onChange={onChange} placeholder="Name must be atleast of 5 characters" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" value={credentials.email} onChange={onChange} placeholder="e.g. sample@gmail.com" />
+              <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+              <input type="password" className="form-control" id="exampleInputPassword1" name="password" value={credentials.password} onChange={onChange} placeholder="Password must be atleast of 5 characters" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">Address</label>
+              <input type="text" className="form-control" id="exampleInputPassword1" name="geolocation" value={credentials.geolocation} onChange={onChange} placeholder="e.g. Delhi"/>
+            </div>
 
 
-        <button type="submit" className="fs-5 m-3 p-2 btn btn-success ">Submit</button>
-        <Link to="/login" className="fs-5 m-3 p-2  btn btn-danger" >Already a user</Link>
-      </form>
-    </div>
+            <button type="submit" className="fs-5 m-3 p-2 btn btn-success ">Submit</button>
+            <Link to="/login" className="fs-5 m-3 p-2  btn btn-danger" >Already a user</Link>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
